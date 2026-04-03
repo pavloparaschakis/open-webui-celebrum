9b9fe8832556611e8e1157651e2ca625dfb2b2d4
Pavlo Paraschakis <pavloparaschakis@gmail.com>
2026-04-03 13:27:16 +0300

    Rebrand to Celebrum

--- src/lib/components/channel/Channel.svelte
+++ src/lib/components/channel/Channel.svelte
@@ -288,10 +288,10 @@
 					} else {
 						return e.name;
 					}
-				}, '')} • Open WebUI</title
+				}, '')} • Celebrum</title
 		>
 	{:else}
-		<title>#{channel?.name ?? 'Channel'} • Open WebUI</title>
+		<title>#{channel?.name ?? 'Channel'} • Celebrum</title>
 	{/if}
 </svelte:head>
 
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -454,7 +454,7 @@
 
 					if ($isLastActiveTab) {
 						if ($settings?.notificationEnabled ?? false) {
-							new Notification(`${displayTitle} • Open WebUI`, {
+							new Notification(`${displayTitle} • Celebrum`, {
 								body: content,
 								icon: `${WEBUI_BASE_URL}/static/favicon.png`
 							});
@@ -657,7 +657,7 @@
 
 				if ($isLastActiveTab) {
 					if ($settings?.notificationEnabled ?? false) {
-						new Notification(`${title} • Open WebUI`, {
+						new Notification(`${title} • Celebrum`, {
 							body: data?.content,
 							icon: `${WEBUI_API_BASE_URL}/users/${data?.user?.id}/profile/image`
 						});
